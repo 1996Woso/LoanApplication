ab25edf769bec7ef0fc4e301db84592a747fad47
EviweKhohlombe <eviwek@joburg.org.za>
2024-10-13 14:28:12 +0200

    First Commit

---
+++ LoanApplictationApp/LoanApplictationApp/Data/Migrations/20241012112126_AddedAppicationUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanApplictationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAppicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfCreation",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfCreation",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ LoanApplictationApp/LoanApplictationApp/Services/LoanApp.cs
@@ -0,0 +1,24 @@
+﻿using LoanApplictationApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanApplictationApp.Services
+{
+    public class LoanApp
+    {
+        private readonly ApplicationDbContext _context;
+        public LoanApp(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<string>> GetAllRolesAsync()
+        {
+            return await _context.Roles.Select(x => x.Name).Distinct().ToListAsync();
+        }
+
+        public async Task<List<string>> GetAllEmailsAsync()
+        {
+            return await _context.Users.Select(x => x.UserName).ToListAsync();
+        }
+    }
+}
